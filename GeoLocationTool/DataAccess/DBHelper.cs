68cafdea09526d555c181c94dc24c69ecb32e259
Nicolas Mathieu <nico008@gmail.com>
2014-02-20 23:34:02 +0000

    Added Helper class to make it easy to create and open the database Added Dapper dependencies

---
+++ GeoLocationTool/DataAccess/DBHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Linq;
+using Dapper;
+
+namespace GeoLocationTool.DataAccess
+{
+    //Not fan of this name
+    internal static class DBHelper
+    {
+        public static SqlCeConnection GetDbConnection(string dbLocation)
+        {
+            var sqlConnection = new SqlCeConnection();
+            sqlConnection.ConnectionString = "Data Source = " + dbLocation;
+
+            if (!File.Exists(dbLocation))
+            {
+                SqlCeEngine engine = new SqlCeEngine(sqlConnection.ConnectionString);
+                engine.CreateDatabase();
+            }
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+
+        public static void InitializeDB(this DbConnection connection)
+        {
+            //Probably not the best way to do it!
+            if (connection.Query<int>("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'NearMatches'").Single() == 0)
+                connection.Execute("CREATE TABLE NearMatches (matchId uniqueidentifier PRIMARY KEY, near nvarchar(255), actual nvarchar(255))");
+        }
+    }
+}
