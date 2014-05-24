9af8b561d5d11c8b8bc2fd7d5f6756e5ffce90e7
stdev34 <st_dev@hotmail.co.uk>
2014-05-24 19:18:19 +0100

    Error handling: constraints added to db
    DB Helper: contraints added to prevent duplicate data  and nulls

--- MultiLevelGeoCoder/DataAccess/DBHelper.cs
+++ MultiLevelGeoCoder/DataAccess/DBHelper.cs
@@ -35,22 +35,25 @@ namespace MultiLevelGeoCoder.DataAccess
             {
                 connection.Execute(@"CREATE TABLE Level1Matches (
                                         Id uniqueidentifier PRIMARY KEY,
-                                        Level1 nvarchar(255),
-                                        AltLevel1 nvarchar(255),
-                                        Weight int)");
+                                        Level1 nvarchar(255) NOT NULL,
+                                        AltLevel1 nvarchar(255) NOT NULL,
+                                        Weight int,
+                                        CONSTRAINT AK_Level1_AltLevel1 UNIQUE (Level1,AltLevel1))");
                 connection.Execute(@"CREATE TABLE Level2Matches (
                                         Id uniqueidentifier PRIMARY KEY,
-                                        Level1 nvarchar(255),
-                                        Level2 nvarchar(255),
-                                        AltLevel2 nvarchar(255),
-                                        Weight int)");
+                                        Level1 nvarchar(255) NOT NULL,
+                                        Level2 nvarchar(255) NOT NULL,
+                                        AltLevel2 nvarchar(255) NOT NULL,
+                                        Weight int,
+                                        CONSTRAINT AK_Level1_Level2_AltLevel2 UNIQUE (Level1, Level2, AltLevel2))");
                 connection.Execute(@"CREATE TABLE Level3Matches (
                                         Id uniqueidentifier PRIMARY KEY,
-                                        Level1 nvarchar(255),
-                                        Level2 nvarchar(255),
-                                        Level3 nvarchar(255),
-                                        AltLevel3 nvarchar(255),
-                                        Weight int)");
+                                        Level1 nvarchar(255) NOT NULL,
+                                        Level2 nvarchar(255) NOT NULL,
+                                        Level3 nvarchar(255) NOT NULL,
+                                        AltLevel3 nvarchar(255) NOT NULL,
+                                        Weight int,
+                                        CONSTRAINT AK_Level1_Level2_Level3_AltLevel3 UNIQUE (Level1, Level2, Level3, AltLevel3))");
             }
 
             if (!connection.TableExists("GazetteerColumnsMapping"))
